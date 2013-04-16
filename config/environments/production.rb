1a170509766564afbf1d4e8990683b65ab4ecc78
FJ <fjohnson347@gmail.com>
2013-04-16 14:53:57 -0400

    removed SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,6 @@ SampleApp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, 
-  # and use secure cookies.
-  config.force_ssl = true
-
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
@@ -31,8 +27,8 @@ SampleApp::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  #Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  #config.force_ssl = true
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
